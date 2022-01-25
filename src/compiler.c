45f4417d18e4a939cc4f259fac4ff44b77a6dbbf
Andrew <andrewnijmeh1@gmail.com>
2022-01-25 14:29:48 -0500

    finish compiler

--- src/compiler.c
+++ src/compiler.c
@@ -38,6 +38,26 @@ void brainfuck(char *command_pointer, char *input) {
       case '[':
         if (!*dp) {
 
+          for (bracket_flag=1; bracket_flag; command_pointer++)
+            if (*command_pointer == '[')
+              bracket_flag++;
+
+            else if (*command_pointer == ']')
+              bracket_flag--;
         }
+        break;
+      case ']':
+
+      if (*dp) {
+          command_pointer -= 2;
+          for (bracket_flag=1; bracket_flag; command_pointer--)
+            if (*command_pointer == ']')
+              bracket_flag++;
+            else if (*command_pointer == '[')
+                bracket_flag--;
+          command_pointer++;
+        }
+      break;
     }
+  printf("\n");
 }
